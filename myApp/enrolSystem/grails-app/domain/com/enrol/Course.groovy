2f8a0b72d172804d66cfea1c1d8a4839bb2e5016
b9013630 <b9013630@my.shu.ac.uk>
2021-02-03 15:36:04 +0000

    Complete task 7 student form added

---
+++ myApp/enrolSystem/grails-app/domain/com/enrol/Course.groovy
@@ -0,0 +1,29 @@
+package com.enrol
+
+class Course {
+
+String department
+String courseTitle
+String courseLeader
+String courseCode
+Date startDate
+Date endDate
+String description
+Integer numberOfStudents
+Double tuitionFees
+
+    static constraints = {
+
+courseTitle blank:false, nullable:false
+department blank:false, nullable:false
+courseLeader blank:false, nullable:false
+courseCode blank:false, nullable:false
+numberOfStudents blank:false, nullable:false, min:20, max:60 
+startDate blank:false, nullable:false
+endDate blank:false, nullable:false
+//studyMode blank:false, nullable:false, size:20
+description blank:false, nullable:false, maxSize:5000, widget:'textarea'
+tuitionFees blank:false, nullable:false, scale:3
+    }
+
+}
