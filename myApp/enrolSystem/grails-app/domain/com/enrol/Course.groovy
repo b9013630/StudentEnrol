2d5bdeb60d5a06d841906096739f2ff3468fa974
b9013630 <b9013630@my.shu.ac.uk>
2021-02-10 15:54:42 +0000

    completed week 4 tasks (relationships). Need to create my own relationships.

--- myApp/enrolSystem/grails-app/domain/com/enrol/Course.groovy
+++ myApp/enrolSystem/grails-app/domain/com/enrol/Course.groovy
@@ -2,29 +2,36 @@ package com.enrol
 
 class Course {
 
-String department
-String courseTitle
-String courseLeader
-String courseCode
-Date startDate
-Date endDate
-String description
-Integer numberOfStudents
-Double tuitionFees
-String studyMode
+	String department
+	String courseTitle
+	String courseLeader
+	String courseCode
+	Date startDate
+	Date endDate
+	String description
+	Integer numberOfStudents
+	Double tuitionFees
+	String studyMode
+
+//Will convert com.enrol.course course 1 to 'coursetitle'
+	String toString(){
+		return courseTitle
+	}
 
     static constraints = {
 
-courseTitle blank:false, nullable:false
-department blank:false, nullable:false
-courseLeader blank:false, nullable:false
-courseCode blank:false, nullable:false
-numberOfStudents blank:false, nullable:false, min:20, max:60 
-startDate blank:false, nullable:false
-endDate blank:false, nullable:false
-studyMode blank:false, nullable:false, size:20
-description blank:false, nullable:false, maxSize:5000, widget:'textarea'
-tuitionFees blank:false, nullable:false, scale:3
+	courseTitle blank:false, nullable:false
+	department blank:false, nullable:false
+	courseLeader blank:false, nullable:false
+	courseCode blank:false, nullable:false
+	numberOfStudents blank:false, nullable:false, min:20, max:60 
+	startDate blank:false, nullable:false
+	endDate blank:false, nullable:false
+	studyMode blank:false, nullable:false, size:1..20
+	description blank:false, nullable:false, maxSize:5000, widget:'textarea'
+	tuitionFees blank:false, nullable:false, scale:3
     }
+
+static hasMany = [students:Student] //1:m relationship
 
 }
--- myApp/enrolSystem/grails-app/domain/com/enrol/Student.groovy
+++ myApp/enrolSystem/grails-app/domain/com/enrol/Student.groovy
@@ -1,26 +1,27 @@
 package com.enrol
 
 class Student {
-
-String studentName
-String studentID
-Date dob
-Boolean isFundingAvailable
-String studentEmail
-String studentUsername
-String studentPassword
-String course
+//declaring variables
+	String studentName
+	String studentID
+	Date dob
+	Boolean isFundingAvailable
+	String studentEmail
+	String studentUsername
+	String studentPassword
+	static belongsTo = [course:Course] //stating the owner of relaionship
 
     static constraints = {
-
-studentName blank:false, nullable:false
-studentID blank:false, nullable:false
-dob blank:false, nullable:false
-studentEmail blank:false, nullable:false, email:true
-studentUsername blank:false, nullable:false, unique:true
-studentPassword blank:false, nullable:false
-isFundingAvailable blank:false, nullable:false
-course blank:false, nullable:false
+//declaring constraints
+	studentName blank:false, nullable:false
+	studentID blank:false, nullable:false
+	dob blank:false, nullable:false
+	studentEmail blank:false, nullable:false, email:true
+	studentUsername blank:false, nullable:false, unique:true
+	studentPassword blank:false, nullable:false
+	isFundingAvailable blank:false, nullable:false
+	//course blank:false, nullable:false
 
     }
+	
 }
