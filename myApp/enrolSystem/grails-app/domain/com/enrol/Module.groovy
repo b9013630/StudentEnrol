289ee502214b6d6d3bd31903e1a6998bd2b6becc
b9013630 <b9013630@my.shu.ac.uk>
2021-03-09 10:42:33 +0000

    Working app on debian machine. had to grant privileges to user.

--- myApp/enrolSystem/grails-app/domain/com/enrol/Module.groovy
+++ myApp/enrolSystem/grails-app/domain/com/enrol/Module.groovy
@@ -17,7 +17,7 @@ class Module {
 	moduleTitle nullable:false, blank:false
 	moduleCode nullable:false, blank:false
 	credits nullable:false, blank:false, min:20, max:120
-	course nullable:false, blank:false
+	//course nullable:false, blank:false
 	description nullable:false, blank:false, size:0..5000, widget:'textarea'
 
     }
